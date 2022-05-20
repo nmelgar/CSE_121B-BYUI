c7b30e17ccdb7cc93454d37e3e5f7dc98988977c
nmelgar <nmelgar16013@gmail.com>
2022-05-20 15:17:17 -0500

    Starting with fetch, async, promises.

---
+++ week5/scripts/fetch.js
@@ -0,0 +1,43 @@
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+const urlList = "https://pokeapi.co/api/v2/pokemon";
+let results = null;
+
+async function getPokemon(url) {
+  const response = await fetch(url);
+  //check to see if the fetch was successful
+  if (response.ok) {
+    // the API will send us JSON...but we have to convert the response before we can use it
+    // .json() also returns a promise...so we await it as well.
+    const data = await response.json();
+    doStuff(data);
+  }
+}
+async function getPokemonList(url) {
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    doStuffList(data);
+  }
+}
+function doStuff(data) {
+  results = data;
+  const outputElement = document.querySelector("#output");
+  const html = `<h2>${data.name}</h2><img src="${data.sprites.front_default}" alt="${data.name}">`;
+  outputElement.innerHTML = html;
+  console.log("first: ", results);
+}
+
+function doStuffList(data) {
+  console.log(data);
+  const pokeListElement = document.querySelector("#outputList");
+  const pokeList = data.results;
+  pokeList.forEach((currentItem) => {
+    const html = `<li>${currentItem.name}</li>`;
+    // note the += here...
+    pokeListElement.innerHTML += html;
+  });
+}
+getPokemon(url);
+console.log("second: ", results);
+
+getPokemonList(urlList);
---
+++ week5/scripts/task5.js
@@ -0,0 +1,91 @@
+/* Lesson 5 */
+
+/* IF/ELSE IF */
+
+// Step 1: Declare and initialize a new variable to hold the current date
+// Step 2: Declare another variable to hold the day of the week
+// Step 3: Using the variable declared in Step 1, assign the value of the variable declared in Step 2 to the day of the week ( hint: getDay() )
+// Step 4: Declare a variable to hold a message that will be displayed
+// Step 5: Using an if statement, if the day of the week is a weekday (i.e. Monday - Friday), set the message variable to the string 'Hang in there!'
+// Step 6: Using an else statement, set the message variable to 'Woohoo!  It is the weekend!'
+const newDay = new Date();
+let day = newDay.getDay();
+
+let dayOutput = document.getElementById("message2")
+
+let message;
+if (day <= 5) {
+    message = "Hang in there";
+    dayOutput.innerHTML = day + ". " + message
+    console.log(message)
+} else {
+    message = "Woohoo! Is is the weekend!"
+}
+/* SWITCH, CASE, BREAK */
+
+// Step 1: Declare a new variable to hold another message
+
+// Step 2: Use switch, case and break to set the message variable to the day of the week as a string (e.g. Sunday, Monday, etc.) using the day of week variable declared in Step 2 above
+
+/* OUTPUT */
+
+// Step 1: Assign the value of the first message variable to the HTML element with an ID of message1
+
+// Step 2: Assign the value of the second message variable to the HTML element with an ID of message2
+
+/* FETCH */
+// Step 1: Declare a global empty array variable to store a list of temples
+let temples = [];
+// Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
+// - Creates an HTML <article> element
+// - Creates an HTML <h3> element and add the temple's templeName property to it
+// - Creates an HTML <h4> element and add the temple's location property to it
+// - Creates an HTML <h4> element and add the temple's dedicated property to it
+// - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
+// - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
+// - Appends the <article> element to the HTML element with an ID of temples
+function output(temples) {
+    let div = document.getElementById("temples")
+    // let ul = '<ul id ="templeList"></ul>';
+    // div.innerHTML = ul;
+    // let ulElement = document.getElementById("templeList")
+    let ul = document.createElement("ul");
+
+
+    temples.forEach(temple => {
+
+
+        let item = document.createElement("li")
+        item.innerHTML = temple.templeName;
+
+        ul.appendChild(item);
+
+    });
+
+    div.appendChild(ul)
+}
+// Step 3: Create another function called getTemples. Make it an async function.
+// Step 4: In the function, using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'. Create a variable to hold the response from your fetch. You should have the program wait on this line until it finishes.
+// Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). Store this in the templeList variable you declared earlier (Step 1). Make sure the the execution of the code waits here as well until it finishes.
+// Step 6: Finally, call the output function and pass it the list of temples. Execute your getTemples function to make sure it works correctly.
+async function getTempleData() {
+    let data = fetch('https://byui-cse.github.io/cse121b-course/week05/temples.json')
+    data = await (await data).json();
+    output(data);
+}
+
+getTempleData();
+
+// Step 7: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
+
+// Step 8: Declare a function named sortBy that does the following:
+// - Calls the reset function
+// - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
+// - Calls the output function passing in the sorted list of temples
+
+// Step 9: Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function
+
+/* STRETCH */
+
+// Consider adding a "Filter by" feature that allows users to filter the list of temples
+// This will require changes to both the HTML and the JavaScript files
