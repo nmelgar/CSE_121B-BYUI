4f2aa740022ba96a92ea7e2d262c7548e8d916e8
nmelgar <nmelgar16013@gmail.com>
2022-05-29 01:03:34 -0500

    Project is done.

--- week6/scripts/main.js
+++ week6/scripts/main.js
@@ -1,50 +1,46 @@
-//  create the quote in html
-// let randomNumber;
-// function getRandom(quotesNumbers){
-//     randomNumber = (Math.floor(Math.random() * quotesNumbers.length));
-//     return randomNumber
-// }
+let quptes = [];
 
-let quotes = []
+function output(quotes) {
+    let quotesContainer = document.getElementById("quotes-container")
+   
+    quotes.forEach(quote => {
 
-function quotesLength(quotesLen){
-    length = quotesLen.length
-    return length
+        let article = document.createElement("article");
+        article.setAttribute("class", "each-quote")
+
+        let quoteParagraph = document.createElement("p")
+        quoteParagraph.setAttribute("class", "quote-paragraph")
+        quoteParagraph.innerHTML = '"' + quote.cita + '"';
+
+        let autorText = document.createElement("div")
+        autorText.setAttribute("class", "quote-autor")
+        autorText.innerHTML = "-" + quote.autor;
+
+        // append different element as childs
+        article.appendChild(quoteParagraph);
+        article.appendChild(autorText);
+        quotesContainer.appendChild(article)
+
+        // create columns for the grid
+        quotesContainer.style.setProperty('grid-template-columns', 'repeat(3, 1fr)');
+
+        // create random colors for backgrounds
+        let colorRed = Math.floor(Math.random() * 256);
+        let colorGreen = Math.floor(Math.random() * 256);
+        let colorBlue = Math.floor(Math.random() * 256);
+        article.style.backgroundColor = 'rgb(' + colorRed + ', ' + colorGreen + ', ' + colorBlue + ')';
+
+    });
 }
 
-
-function newQuote(quotes) {
-    // randomNumber = (Math.floor(Math.random() * 2));
-
-    let quotesContainer = document.querySelector(".quotes-container");
-
-    let quoteParagraph = document.createElement("h3")
-    quoteParagraph.innerHTML = '"' + quotes[1].cita + '"';
-    quotesContainer.appendChild(quoteParagraph)
-
-}
-
-// function newQuote(quotes){
-//     let generateQuote = quotes[Math.floor(Math.random() * quotes.length)];
-//     let printQuote = document.getElementById('quote-here');
-//     printQuote.textContent = generateQuote;
-// }
-
-
-//  to get the quotes
+// async function to get the quotes
 let getQuotes = async () => {
     let response = await fetch(
         "https://nmelgar.github.io/citas_lds_spanish_json/citas_lds_spanish.json"
     );
     quotesData = await response.json();
-    
-    //this returns a promise
-    newQuote(quotesData);
-    quotesLength(quotesData);
+
+    // returns a promise
+    output(quotesData);
 };
 getQuotes();
-
-let randomQuote = document.getElementById('new-quote').addEventListener("click", newQuote);
-// document.getElementById("new-quote").onclick = function () {
-//     showQuotes()
-// };
